e46ce0c4bf1e48ffc94bbedbc8c51e472be23bd6
sonuyadav <sonnu.yadav@gmail.com>
2018-01-29 19:29:28 +0530

    fist commit

---
+++ function.js
@@ -0,0 +1,27 @@
+// function myName(name){
+// console.log(name)
+// }
+
+// myName('sonu manish')
+// var num = 1
+// function factorial(){
+// num++    
+// console.log(num);
+// factorial();
+// }
+
+//factorial();
+//console.log(factorial(5))
+//console.log(typeof(factor));
+
+for(var i=1; i<=100; i++){
+
+    if(i%3 ==0){
+        console.log('Fish');
+    }
+
+    if(i%5==0){
+        console.log('sonu')
+    }
+    console.log(i)
+}
